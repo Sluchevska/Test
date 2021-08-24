2093c4eb46a23e78691d60a0a2ec5ea1b6aa91be
Sluchevska <alaska3a@gmail.com>
2021-08-24 20:20:43 +0100

    update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,8 +5,8 @@ import user from './components/social-profile/user.json';
 import Profile from './components/social-profile/Profile';
 import Statistics from './components/statistics/statistics';
 import statisticalData from './components/statistics/statistical-data.json';
-import TransactionHistory from './transaction-history/TransactionHistory';
-import transactions from './transaction-history/transactions.json'
+import TransactionHistory from './components/transaction-history/TransactionHistory';
+import transactions from './components/transaction-history/transactions.json'
 
 function App() {
   return (
