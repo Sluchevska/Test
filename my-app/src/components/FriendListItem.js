bbcf60a50710ac005b9242c937a339816f5ce268
Sluchevska <alaska3a@gmail.com>
2021-08-24 14:04:43 +0100

    update

--- my-app/src/components/FriendListItem.js
+++ my-app/src/components/FriendListItem.js
@@ -2,10 +2,10 @@ import PropTypes from 'prop-types'
 
 export default function FriendListItem(props) {
   const {avatar, name, isOnline} = props
-  return <li>
-    <span>{ isOnline}</span>
-  <img src={avatar} alt={name} width="48" />
-    <p>{ name}</p>
+  return <li class="item">
+    <span class="status">{ isOnline}</span>
+  <img class="avatar" src={avatar} alt={name} width="48" />
+    <p class="name">{ name}</p>
 </li>
 }
 
